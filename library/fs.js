8efe8999f201e320bd626744d99358c41870465b
summer <18855176517@163.com>
2020-08-29 14:40:38 +0800

    server

--- library/fs.js
+++ library/fs.js
@@ -3,11 +3,11 @@ const fs = require('fs') // 文件数据，系统自带模块
 // fs.watchFile(path, data, callback) 写入文件 路径 数据 回调
 // fs.readFile(path, data, callback) 读取文件 路径 回调
 
-fs.writeFile('../demo.txt', '13', err => {
+fs.writeFile('../www/demo.txt', '13', err => {
   if (err) console.log(err)
 })
 
-fs.readFile('../demo.txt', (err, data) => {
+fs.readFile('../www/demo.txt', (err, data) => {
   if (err) return console.log(err)
   console.log(data)
 })
--- post_file/server_file.js
+++ post_file/server_file.js
@@ -25,61 +25,64 @@ const tools = {
 // ]
 
 http.createServer((req, res) => {
-  console.log(req.headers['content-type'])
-  let boundary
-  if (req.headers['content-type']) {
-    boundary = '--' + req.headers['content-type'].split('; ')[1].split('=')[1] // ------WebKitFormBoundary2IHpQ6Aml3OFmnKu
-  }
-  
-  let arr = []
-  req.on('data', buffer => {
-    arr.push(buffer)
-  })
-  req.on('end', () => {
-    let buffer = Buffer.concat(arr)
-
-    // 按照分隔符切分
-    let resAll = tools.bufferSplit(buffer, boundary) // 首尾是null，'--'
-    resAll.pop()
-    resAll.shift()
-    // console.log(resAll.toString())
-
-    // 每一个进行处理
-    resAll.forEach(buffer => {
-      buffer = buffer.slice(2, buffer.length - 2) // 去除首尾 \r\n
-
-      let n = buffer.indexOf('\r\n\r\n')
-
-      let field = buffer.slice(0, n).toString()
-      let data = buffer.slice(n + 4)
-      
-      if (field.indexOf('\r\n') !== -1) { // 文件会有两行信息，包含 \r\n
-        let field2 = field.split('\r\n')[0].split('; ')
-        let name = field2[1].split('=')[1]
-        let filename = field2[2].split('=')[1]
-
-        name = name.substring(1, name.length - 1)
-        filename = filename.substring(1, filename.length - 1)
-        // console.log(name, filename)
-        if (filename) {
-          fs.writeFile(`upload/${filename}`, data, err => {
-            if (err) {
-              console.log(err)
-            } else {
-              console.log('上传成功')
-            }
-          })
-        }
-        
-      } else { // 普通信息
-        let name = field.split('; ')[1].split('=')[1]
-        name = name.substring(1, name.length - 1) // 去除首尾引号
-        // console.log(name, data.toString())
-      }
-
+  console.log(req.url)
+  if (req.url === '/upload') {
+    console.log(req.headers['content-type'])
+    let boundary
+    if (req.headers['content-type']) {
+      boundary = '--' + req.headers['content-type'].split('; ')[1].split('=')[1] // ------WebKitFormBoundary2IHpQ6Aml3OFmnKu
+    }
+    
+    let arr = []
+    req.on('data', buffer => {
+      arr.push(buffer)
     })
-    // res.write('200')
-    // res.end()
-  })
+    req.on('end', () => {
+      let buffer = Buffer.concat(arr)
+
+      // 按照分隔符切分
+      let resAll = tools.bufferSplit(buffer, boundary) // 首尾是null，'--'
+      resAll.pop()
+      resAll.shift()
+      // console.log(resAll.toString())
+
+      // 每一个进行处理
+      resAll.forEach(buffer => {
+        buffer = buffer.slice(2, buffer.length - 2) // 去除首尾 \r\n
+
+        let n = buffer.indexOf('\r\n\r\n')
+
+        let field = buffer.slice(0, n).toString()
+        let data = buffer.slice(n + 4)
+        
+        if (field.indexOf('\r\n') !== -1) { // 文件会有两行信息，包含 \r\n
+          let field2 = field.split('\r\n')[0].split('; ')
+          let name = field2[1].split('=')[1]
+          let filename = field2[2].split('=')[1]
+
+          name = name.substring(1, name.length - 1)
+          filename = filename.substring(1, filename.length - 1)
+          // console.log(name, filename)
+          if (filename) {
+            fs.writeFile(`upload/${filename}`, data, err => {
+              if (err) {
+                console.log(err)
+              } else {
+                console.log('上传成功')
+              }
+            })
+          }
+          
+        } else { // 普通信息
+          let name = field.split('; ')[1].split('=')[1]
+          name = name.substring(1, name.length - 1) // 去除首尾引号
+          // console.log(name, data.toString())
+        }
+
+      })
+      res.write('200')
+      res.end()
+    })
+  }
 
 }).listen(3030)
