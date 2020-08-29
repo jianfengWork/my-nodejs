65e9ecbec1ebd6e2973ac499656fd01cd9bb1599
summer <18855176517@163.com>
2020-08-29 15:21:09 +0800

    formdata

--- http.js
+++ http.js
@@ -1,17 +1,17 @@
 const http = require('http')
 const fs = require('fs')
 
-let server = http.createServer((request, response) => { // 请求 request => 请求来的数据，get，post以及参数 response => 返回结果
+let server = http.createServer((req, res) => { // 请求 req => 请求来的数据，get，post以及参数 res => 返回结果
   // console.log('有请求')
-  console.log(request.url) // http://localhost:3030/index.html => /index.html
-  if (request.url === '/index.html') {
+  console.log(req.url) // http://localhost:3030/index.html => /index.html
+  if (req.url === '/index.html') {
     fs.readFile('www/index.html', (err, data) => {
-      response.writeHead(200) // 写入状态码
-      response.write(data)
-      response.end()
+      res.writeHead(200) // 写入状态码
+      res.write(data)
+      res.end()
     })
   } else {
-    response.end()
+    res.end()
   }
 })
 
