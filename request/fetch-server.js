62ac554b53af20b8f676651cc68eb81dbde8d7cb
summer <18855176517@163.com>
2020-09-10 15:28:11 +0800

    fetch-server

---
+++ request/fetch-server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+http.createServer((req, res) => {
+  console.log(req.url)
+  const { pathname } = url.parse(req.url, true)
+  if (pathname !== '/favicon.ico') {
+    fs.readFile('.' + pathname, (err, buffer) => {
+      if (err) {
+        res.writeHeader(404)
+        res.write('not found')
+      } else {
+        res.write(buffer)
+      }
+      res.end()
+    })
+  }
+}).listen(3030)
