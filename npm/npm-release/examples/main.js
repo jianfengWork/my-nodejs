61bc773ce817e8ee576128aeec3d41da12b83a5f
summer <18855176517@163.com>
2020-10-17 21:37:42 +0800

    npm-release

---
+++ npm/npm-release/examples/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import hljs from 'highlight.js'
+import 'highlight.js/styles/railscasts.css'
+
+// http://www.rxshc.com/180.html
+import ColorPicker from './../packages/index'
+
+Vue.directive('hljs', el => {
+  let blocks = el.querySelectorAll('pre')
+  Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+})
+
+Vue.use(ColorPicker)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
