cf9a1a3164226dfd03e4616274f407479d22672b
summer <18855176517@163.com>
2020-12-22 15:32:17 +0800

    fs

--- library/fs.js
+++ library/fs.js
@@ -1,7 +1,9 @@
 const fs = require('fs') // 文件数据，系统自带模块
 const path = require('path');
 
+// 带 Sync 是 同步，不带是 异步
 // fs.writeFile(path, data, callback) 写入文件 路径 数据 回调
+// fs.writeFileSync(path, data, callback) 写入文件 路径 数据 回调
 // fs.readFile(path, data, callback) 读取文件 路径 回调
 
 fs.writeFile('../www/demo.txt', '13', err => { // 写入数据并创建文件
@@ -12,6 +14,36 @@ fs.readFile('../www/demo.txt', (err, data) => {
   if (err) return console.log(err)
   console.log(data)
 })
+
+console.log(1);
+fs.stat('../www/demo.txt', (err, stat) => {
+  // 一般回调函数的第一个参数是错误对象，如果err为null,表示没有错误，否则表示报错了
+  if (err) return;
+  if (stat.isFile()) {
+    console.log('文件');
+  } else if (stat.isDirectory()) {
+    console.log('目录');
+  }
+  console.log(stat);
+  /*
+    stat.atime 文件访问时间
+    stat.ctime 文件的状态信息发生变化的时间（比如文件的权限）
+    stat.mtime 文件数据发生变化的时间
+    stat.birthtime 文件创建的时间
+  */
+  console.log(2);
+});
+console.log(3);
+// 1, 3, 2
+
+/*
+  同步操作
+  console.log(1);
+  let ret = fs.statSync('../www/demo.txt');
+  console.log(ret);
+  console.log(2);
+  // 1, ret, 2
+*/
 
 /*
   文件操作案例（初始化目录结构）
--- library/stream.js
+++ library/stream.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 
+// writeFile 和 readFile 会先写入内存，导致内存不足
+// createReadStream 和 createWriteStream，数据流，一部份 一部份 进行处理，大文件操作
+
 // const rs = fs.createReadStream('../www/avatar.png') // 读入流
 // const ws = fs.createWriteStream('avatar.copy.png') // 写出流
 
-// rs.pipe(ws) // 管道接入
+// rs.pipe(ws) // 管道接入，输入流(磁盘到内存) 和 输出流(内存到磁盘) 连接到一起
 
 // rs.on('error', err => { // 处理错误，防止服务器蹦掉
 //   console.log(err)
