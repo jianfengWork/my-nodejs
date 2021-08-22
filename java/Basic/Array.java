dfe9edccc7e786e3374b0f7ac565a6b892565a87
summer <18855176517@163.com>
2021-08-22 13:31:23 +0800

    java Array

---
+++ java/Basic/Array.java
@@ -0,0 +1,21 @@
+class ArrayDemo {
+  public static void main(String[] args) {
+    // 定义数组
+    int[] arr = {28,55,37,46,19};
+    
+    // 遍历
+    printArray(arr);
+  }
+
+  public static void printArray(int[] arr) {
+    System.out.print("[");
+    for (int x = 0; x < arr.length; x++) {
+      if (x == arr.length - 1) {
+        System.out.println(arr[x] + "]");
+      } else {
+        System.out.print(arr[x] + ", ");
+      }
+    }
+  }
+
+}
