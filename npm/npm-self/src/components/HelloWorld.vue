263eb5dc0dd04301940c08e100c43ffccf531b40
jianfeng <18855176517@163.com>
2023-09-26 09:46:31 +0800

    del vue-cli1.0

--- npm/npm-self/src/components/HelloWorld.vue
+++ npm/npm-self/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <vtitle title="你是傻逼" @myclick="printVal" />
+    <vtitle title="hello world" @myclick="printVal" />
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
